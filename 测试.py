00a5b967485e7d6fc9f89ffe797b27d73936aa5b
北辰 <3857327758@qq.com>
2025-11-29 01:19:04 +0800

    Update 测试.py

--- 测试.py
+++ 测试.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client()
+client = genai.Client(api_key="AIzaSyBe2kqb8XmD3nMg2ER38n_aPoDaqtr079g")
 
 response = client.models.generate_content(
     model="gemini-3-pro-preview",
